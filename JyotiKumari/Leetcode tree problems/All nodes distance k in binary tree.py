0a2b031abdf8b49b1a5fb28d23b1f9e25fd37d2d
jaiswaljyoti <jaiswaljyoti2000@gmail.com>
2022-02-21 23:44:45 +0530

    https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/submissions/

---
+++ JyotiKumari/Leetcode tree problems/All nodes distance k in binary tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        def dfs(parent, child, neighbors):
+            if not child:
+                return
+            if parent:
+                neighbors[parent.val].append(child.val)
+                neighbors[child.val].append(parent.val)
+            dfs(child, child.left, neighbors)
+            dfs(child, child.right, neighbors)
+
+        neighbors = collections.defaultdict(list)
+        dfs(None, root, neighbors)
+        bfs = [target.val]
+        lookup = set(bfs)
+        for _ in range(k):
+            bfs = [nei for node in bfs
+                   for nei in neighbors[node]
+                   if nei not in lookup]
+            lookup |= set(bfs)
+        return bfs
